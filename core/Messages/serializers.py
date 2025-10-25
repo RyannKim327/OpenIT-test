2db51cb55c02c5a04a74c474515ea88c484a078c
RyannKim327 <rksesgundo123@gmail.com>
2025-10-25 11:50:55 +0800

    10-25-25 11:50 [Fixed error]

--- core/Messages/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.serializers import Serializer
-
-from Messages.model import Message
-
-
-class MessageSerializer(Serializer):
-    class Meta:
-        model = Message
-        fields = "__all__"
