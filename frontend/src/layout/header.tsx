881b89f7beb2a9a8273a405de0ce9d7baeb5262e
RyannKim327 <rksesgundo123@gmail.com>
2025-10-26 09:34:09 +0800

    10-26-25  9:34 [https://www.youtube.com/watch\?v\=cvh0nX08nRw]

--- frontend/src/layout/header.tsx
+++ frontend/src/layout/header.tsx
@@ -1,17 +1,50 @@
-import { Home, Mail } from "lucide-react";
-import { Link } from "react-router";
+import { Home, Mail, User } from "lucide-react";
+import { Link, useLocation } from "react-router";
 import Logo from "./../assets/logo.png";
 
 export default function Header() {
+  const location = useLocation();
+
   return (
     <div className="flex flex-row gap-1 justify-between items-center px-2">
-      <img src={Logo} height={50} width={50} />
-      <div className="flex flex-row gap-1">
-        <Link to="/" title="Home">
-          <Home size={24} color="#6E43EB" />
+      <div className="flex flex-row gap-1 items-center">
+        <img src={Logo} height={50} width={50} />
+        <h1 className="text-[#509A92] text-shadow-sm text-shadow-gray-300 font-bold">
+          SereneMind Hub
+        </h1>
+      </div>
+      <div className="flex flex-row gap-2">
+        <Link
+          to="/"
+          className={`flex flex-col items-center justify-center font-bold ${location.pathname === "/" || location.pathname === "" ? "text-[#509A92]" : "text-gray-400"}`}
+          title="Home"
+        >
+          <Home size={24} />
+          <span>Home</span>
         </Link>
-        <Link to="/messages" title="Messages">
-          <Mail size={24} color="#6E43EB" />
+        <Link
+          to="/messages"
+          className={`flex flex-col items-center justify-center font-bold ${location.pathname === "/messages" ? "text-[#509A92]" : "text-gray-400"}`}
+          title="Messages"
+        >
+          <Mail size={24} />
+          <span>Messages</span>
+        </Link>
+        <Link
+          to="/profile"
+          className={`flex flex-col items-center justify-center font-bold ${location.pathname === "/profile" ? "text-[#509A92]" : "text-gray-400"}`}
+          title="Profile"
+        >
+          <User size={24} />
+          <span>Profile</span>
+        </Link>
+      </div>
+      <div className="flex flex-row gap-2 items-center">
+        <Link to="/login" className="text-[##509A92]">
+          Log In
+        </Link>
+        <Link to="/register" className="bg-[#509A92] text-white p-1 roundedtex">
+          Sign Up
         </Link>
       </div>
     </div>
