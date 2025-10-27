836713cddcde051f58f50f96aa035260d57c9e20
Abdul Barry A. Adam <barryadam615@gmail.com>
2025-10-27 09:27:55 +0800

    fixed the notification for multi-read-status handling

--- backend/Notifications/models.py
+++ backend/Notifications/models.py
@@ -6,13 +6,23 @@ from Users.models import User
 
 class Notification(BaseAuthModel):
     order = "notification_added"
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="notifications"
-    )
     content = models.CharField(max_length=80)
-    read = models.BooleanField(default=False)
     notification_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{id}'
     
+# added through model for per-user read status hahahaha
+# this let us trach each users read status
+class UserNotification(BaseAuthModel):
+    order = "read_at"
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_notification')
+    notification = models.ForeignKey(Notification, on_delete=models.CASCADE)
+    read = models.BooleanField(default=False)
+    read_at = models.DateTimeField(null=True, blank=True)
+    class Meta:
+        unique_together = ('user','notification')
+
+    def __str__(self):
+        return f'{id}'
--- backend/Posts/signals.py
+++ backend/Posts/signals.py
@@ -1,24 +1,39 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from Posts.models import Comment
-from Notifications.models import Notification
-
+from Notifications.models import Notification, UserNotification
+from Users.models import User
 
 @receiver(post_save, sender=Comment)
 def new_comment(sender, instance, created,**kwargs):
     if not created:
         return
 
-    user = instance.comment_as_user
-    poster = instance.comment_on_post.user
+    post = instance.post
+    commenter = instance.user
     
-    # avoid notifying the poster when he comments on his own post
-    if user == poster:
-        return
 
-    Notification.objects.create(
-        user=poster,
-        content=f'{user.middle_name} commented on your post "{instance.comment_on_post.title}"'
+    notif = Notification.objects.create(
+        content=f"New comment on {instance.comment_on_post.title}"
     )
+
+    # get all the commenters (excluding or avoiding the one who commented)
+    commenters = User.objects.filter(
+        comments__post=post
+    ).exclude(id=commenter.id).distinct()
+
+    # Include post owner if they’re not the commenter
+    if post.user != commenter:
+        commenters = commenters.union(User.objects.filter(id=post.user.id))
+
+    if not commenters.exists():
+        return
+    
+    notif = Notification.objects.create(content=f"{commenter} also commented on {post.title}")
+
+    for user in commenters:
+        UserNotification.objects.create(user=user, notification=notif)
+
+
 
 
